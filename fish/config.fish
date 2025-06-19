f59a7099eadaa2b947f2a7d62fa4a5bb03dce458
egoreast <egoreast.true@gmail.com>
2025-06-19 13:04:45 +0300

    update download from youtube alias

--- fish/config.fish
+++ fish/config.fish
@@ -39,7 +39,7 @@ alias browser='yandex-browser-stable'
 alias calendar='calcurse'
 alias disk_usage='gdu'
 # download with 128x720 resolution and to Downloads folder
-alias download_from_youtube='yt-dlp -f 136 -P "~/Загрузки/Youtube/"'
+alias download_from_youtube='yt-dlp -f "bestvideo[height<=720]+bestaudio[abr<=128]" --merge-output-format mp4 -o "~/Загрузки/Youtube/%(title)s.%(ext)s"'
 alias enable_keyboard1="sudo chmod 777 /dev/hidraw1"
 alias enable_keyboard2="sudo chmod 777 /dev/hidraw2"
 alias g="lazygit"
