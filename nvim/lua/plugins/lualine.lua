e4a36f0fce6d81d9f7d27df9cd421b70cb1c7bc0
egoreast <i@egoreast.ru>
2025-06-26 22:54:01 +0300

    correct lualine plugin opts

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,25 +1,24 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    sections = {
-      lualine_x = {
-        function()
-          local ok, pomo = pcall(require, "pomo")
-          if not ok then
-            return ""
-          end
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_b, {
+      function()
+        local ok, pomo = pcall(require, "pomo")
+        if not ok then
+          return ""
+        end
 
-          local timer = pomo.get_first_to_finish()
-          if timer == nil then
-            return ""
-          end
+        local timer = pomo.get_first_to_finish()
+        if timer == nil then
+          return ""
+        end
 
-          return "󰄉 " .. tostring(timer)
-        end,
-        "encoding",
-        "fileformat",
-        "filetype",
-      },
-    },
-  },
+        return "󰄉 " .. tostring(timer)
+      end,
+      "encoding",
+      "fileformat",
+      "filetype",
+    })
+    return opts
+  end,
 }
